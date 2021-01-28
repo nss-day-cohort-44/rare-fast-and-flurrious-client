07676011af30c4a9a6fecde231f7eaab21ee9707
Sanjeet Prasad <sanjeet.l.prasad@gmail.com>
2021-01-28 15:56:46 -0600

    added the post

---
+++ src/components/posts/PostProvider.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { useState } from "react"
+
+export const PostContext = React.createContext()
+
+export const PostProvider = (props) => {
+    const [posts, setPosts] = useState([])
+
+    const getPosts = () => {
+        return fetch("http://localhost:8088/posts")
+        .then(r => r.json())
+        .then(setPosts)
+    }
+
+    return(
+        <PostContext.Provider value={{posts,setPosts, getPosts}}>
+            {props.children}
+
+        </PostContext.Provider>
+    )
+}
