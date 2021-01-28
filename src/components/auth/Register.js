917c21bfe44b2027db82809888a092b97840016b
Sanjeet Prasad <sanjeet.l.prasad@gmail.com>
2021-01-28 14:05:23 -0600

    commit

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,5 +1,5 @@
 import React, { useRef } from "react"
-import { Link } from "react-router-dom"
+import { Link, useHistory } from "react-router-dom"
 import "./Auth.css"
 
 export const Register = (props) => {
@@ -10,6 +10,7 @@ export const Register = (props) => {
     const password = useRef()
     const verifyPassword = useRef()
     const passwordDialog = useRef()
+    const history = useHistory()
 
     const handleRegister = (e) => {
         e.preventDefault()
@@ -35,7 +36,7 @@ export const Register = (props) => {
                 .then(res => {
                     if ("valid" in res && res.valid) {
                         localStorage.setItem("rare_user_id", res.token)
-                        props.history.push("/")
+                        history.push("/")
                     }
                 })
         } else {
