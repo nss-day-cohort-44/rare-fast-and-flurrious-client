40907d56fa5ac6d3e33b313a070e9b53c3fdecee
Sanjeet Prasad <sanjeet.l.prasad@gmail.com>
2021-02-23 14:22:49 -0600

    delete dialog box is working fine

--- src/components/tags/Tag.js
+++ src/components/tags/Tag.js
@@ -1,11 +1,46 @@
-import React, {useContext} from "react";
+import React, {useContext, useRef} from "react";
 import { TagContext } from "./TagProvider"
 
 
 export const Tag = ({ tag, props}) => {
   const {deleteTag} = useContext(TagContext)
+  // Delete, if you want as a window popup 
+  // const confirmDelete = () => {
+  //   const d = window.confirm("Are you sure, you want to delete this Tag?")
+  //   if (d === true) {
+  //     deleteTag(tag.id)
+  //   } 
+
+  //}
+
+  // Delete As a Dialog
+  const dialog = useRef()
+
   return (
     <section>
+      <div style={{height: "200px"}, {fontSize: "14px"}}>
+      <dialog ref= {dialog}>
+        <div>
+          Are you sure, you want to delete this Tag?
+
+          <button className="btn--release"
+                  onClick={() => {
+                  
+                    deleteTag(tag.id)
+         
+                     }}
+              >Delete Tag</button>
+              <br></br>
+                  <button className="btn--release"
+                  onClick={() => {
+
+                    dialog.current.close()
+                   }}
+              >Cancel Delete</button>
+        </div>
+      </dialog>
+      </div>
+      
       <p>{tag.label}</p> 
 
       <button onClick={() => {
@@ -15,11 +50,14 @@ export const Tag = ({ tag, props}) => {
 
       <button className="btn--release"
                   onClick={() => {
-                  //releaseAnimal(props.match.params.animalId)
-                    deleteTag(tag.id)
-                // .then(() => {
-                //     props.history.push("/tags")
-                // })
+                   // plain Delete
+                    // deleteTag(tag.id)
+
+                    // Delete, if you want as a window popup
+                    // confirmDelete()
+
+                    dialog.current.showModal()
+         
                      }}
               >Delete Tag</button>
     </section>
